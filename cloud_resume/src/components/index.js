4266779566a558f5999641d37c91feb95b900c17
Kai Brown <104911237+kaibrown28@users.noreply.github.com>
2023-05-03 19:57:54 +0000

    started working on routing

--- cloud_resume/src/components/index.js
+++ cloud_resume/src/components/index.js
@@ -1,6 +1,6 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import Resume from "../components/Resume";
-import Counter from "../components/Counter";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header";
+// import Resume from "../components/Resume";
+// import Counter from "../components/Counter";
 
-export default index
+// export default index
--- cloud_resume/src/index.js
+++ cloud_resume/src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import {ResumePage} from './components/ResumePage';
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <App />,
+  errorElement: <ErrorPage />
+  }, 
+  {
+    path: "/resume",
+    element: <ResumePage />
+  },
+  { 
+    path: "/home",
+    element: <Home />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
